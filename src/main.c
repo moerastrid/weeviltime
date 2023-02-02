ccf90fc15546f2018e8f08d9cc544e0076165687
Astrid Geels <moerastrid@gmail.com>
2023-02-02 15:03:35 +0100

    temp

--- src/main.c
+++ src/main.c
@@ -6,14 +6,57 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/02 14:10:33 by ageels        #+#    #+#                 */
-/*   Updated: 2023/02/02 14:48:39 by ageels        ########   odam.nl         */
+/*   Updated: 2023/02/02 14:54:42 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header.h"
 
+//int32_t	main(void)
+//{
+//	printf("it's weeviltime!\n");
+//	return (0);
+//}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include "../header.h"
+#define WIDTH 512
+#define HEIGHT 512
+
+static mlx_image_t* img;
+
+void hook(void* param)
+{
+	const mlx_t* mlx = param;
+
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		img->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		img->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		img->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		img->instances[0].x += 5;
+}
+
 int32_t	main(void)
 {
-	printf("it's weeviltime!\n");
-	return (0);
+	mlx_t* mlx;
+
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
+		return(EXIT_FAILURE);
+
+	img = mlx_new_image(mlx, 128, 128);
+	memset(img->pixels, 255, img->width * img->height * sizeof(int));
+	mlx_image_to_window(mlx, img, 0, 0);
+
+	mlx_loop_hook(mlx, &hook, mlx);
+	mlx_loop(mlx);
+
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
 }
