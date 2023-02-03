3e791c8f76dd9b434929addc32acc9011228794d
Astrid Geels <moerastrid@gmail.com>
2023-02-03 13:51:14 +0100

    temp

--- src/main.c
+++ src/main.c
@@ -6,13 +6,11 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/02 14:10:33 by ageels        #+#    #+#                 */
-/*   Updated: 2023/02/02 21:50:54 by ageels        ########   odam.nl         */
+/*   Updated: 2023/02/03 13:48:53 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/header.h"
-#define WIDTH 1024
-#define HEIGHT 1024
 
 int	main(int argc, char **argv)
 {
@@ -20,10 +18,9 @@ int	main(int argc, char **argv)
 
 	if (parse(argc, argv, &data))
 		return (1);
-	data.mlx = mlx_init(WIDTH, HEIGHT, "~Weevil time~", true);
+	data.mlx = mlx_init(WIDTH, HEIGHT, "~Weevil time~", false);
 	if (!data.mlx)
 		return (1);
-	printf("MLX ptr inside main: %p\n", &data.mlx);
 	if (!display(&data))
 		return (1);
 	mlx_loop(data.mlx);
