9c8bf9e26ff9b838d26629246fb93f6911881836
Astrid Geels <moerastrid@gmail.com>
2023-02-06 17:57:38 +0100

    add vars to draw_cross

--- src/display/grid.c
+++ src/display/grid.c
@@ -6,7 +6,7 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/03 16:07:07 by ageels        #+#    #+#                 */
-/*   Updated: 2023/02/06 17:18:37 by ageels        ########   odam.nl         */
+/*   Updated: 2023/02/06 17:56:57 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,21 @@ void	draw_cube(mlx_image_t *grid, t_co c)
 	}
 }
 
-void	draw_cross(mlx_image_t	*grid)
+void	draw_cross(mlx_image_t	*grid, int var_x, int var_y)
 {
 	t_co	upleft;
 	t_co	upright;
 	t_co	downleft;
 	t_co	downright;
 
-	upleft.x = 0;
-	upleft.y = 0;
-	upright.x = WIDTH;
-	upright.y = 0;
-	downleft.x = 0;
-	downleft.y = HEIGHT;
-	downright.x = WIDTH;
-	downright.y = HEIGHT;
+	upleft.x = var_x;
+	upleft.y = var_y;
+	upright.x = WIDTH - var_x;
+	upright.y = var_y;
+	downleft.x = var_x;
+	downleft.y = HEIGHT - var_y;
+	downright.x = WIDTH - var_x;
+	downright.y = HEIGHT - var_y;
 	line(grid, &upleft, &downright);
 	line(grid, &upright, &downleft);
 }
@@ -50,9 +50,14 @@ void	grid(t_data *data)
 	t_co	c;
 
 	grid = mlx_new_image(data->mlx, WIDTH, HEIGHT);
-	draw_cross(grid);
+	draw_cross(grid, 0, 0);
+	draw_cross(grid, 0, 200);
+	draw_cross(grid, 300, 0);
 	c.x = 200;
 	c.y = 100;
+	draw_cube(grid, c);
+	c.x = 300;
+	c.y = 300;
 	draw_cube(grid, c);
 	mlx_image_to_window(data->mlx, grid, 0, 0);
 }
